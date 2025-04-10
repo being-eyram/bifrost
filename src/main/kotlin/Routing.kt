9804fac309fc90e104bcac07ce141535f7f5eea4
Eyram Hlorgbe <eyram.hlorgbe@hubtel.com>
2025-04-10 09:56:48 +0000

    Update baseUrl

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -51,7 +51,7 @@ fun Application.configureRouting(firebaseApp: FirebaseApp? = null) {
                 //make this http when testing on local
                 protocol = URLProtocol.HTTPS
                 encodedPath = ""
-            }.host
+            }.buildString()
 
             val uploadUrl = "$baseUrl$uploadPath"
 
